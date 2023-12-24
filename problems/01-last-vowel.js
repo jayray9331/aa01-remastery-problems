6c64df85344f106605544c1c925836bf19436964
jayray9331 <79432975+jayray9331@users.noreply.github.com>
2023-12-23 22:51:21 -0500

    Update 01-last-vowel.js

--- problems/01-last-vowel.js
+++ problems/01-last-vowel.js
@@ -10,7 +10,45 @@ String.toUpperCase: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Refe
 */
 
 
-// Your code here 
+/*
+Write a function `lastVowel(str)` that takes in a string and returns the last
+vowel that appears sequentially in the string. Note that the string may contain
+capitalization.
+
+Hint: You may find the `String.toLowerCase` or `String.toUpperCase` methods useful.
+
+String.toLowerCase: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+String.toUpperCase: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+*/
+
+
+ let lastVowel = function(str) {
+    let vowels = 'aeiou';
+  
+    for (let index = str.length - 1; index >= 0; index--) {
+         let letter = str[index];
+         if (vowels.includes(letter.toLowerCase()))  {
+      		return letter;
+    }
+  }
+  
+  return null;
+};
+      
+
+
+console.log(lastVowel('battery')); // 'e'
+console.log(lastVowel('TUNNEL')); // 'E'
+console.log(lastVowel('dog')); // 'o'
+console.log(lastVowel('conventional')); // 'a'
+console.log(lastVowel('rhythm')); // null
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+    module.exports = lastVowel;
+  } catch (e) {
+    module.exports = null;
+  }
 
 
 console.log(lastVowel('battery')); // 'e'
